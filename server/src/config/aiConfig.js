fecc635ae93b00c7e0e76ed6695aa4c62513e549
hey-d <dushyantmanghani@gmail.com>
2026-05-25 15:41:59 +0530

    model change

--- server/src/config/aiConfig.js
+++ server/src/config/aiConfig.js
@@ -1,9 +1,9 @@
-const {GoogleGenerativeAI} = require('@google/generative-ai')
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-require('dotenv').config()
+require("dotenv").config();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const geminiModel = genAI.getGenerativeModel({model: 'gemini-3-flash-preview'});
+const geminiModel = genAI.getGenerativeModel({ model: "gemini-3.5-flash" });
 
 module.exports = geminiModel;
