1473e688b1818a7c943532d4964133650e1f1773
hey-d <dushyantmanghani@gmail.com>
2026-05-25 11:13:21 +0530

    backend url updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const acessurl = "http://localhost:5000";
+const acessurl = "http://murderm-backend.onrender.com/";
 const socket = io(acessurl);
 
 function App() {
