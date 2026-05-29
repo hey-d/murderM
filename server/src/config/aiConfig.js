8d25e56c27604970a710c15f17cd08c0bd78a6fa
hey-d <dushyantmanghani@gmail.com>
2026-05-29 20:35:11 +0530

    model changed to 2.5

--- server/src/config/aiConfig.js
+++ server/src/config/aiConfig.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const geminiModel = genAI.getGenerativeModel({ model: "gemini-3.5-flash" });
+const geminiModel = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 module.exports = geminiModel;
